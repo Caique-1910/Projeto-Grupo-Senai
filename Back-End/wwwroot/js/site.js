cad914c7e09eb4a8f1faaa0ac4079aeefb1acbf6
CAIQUE LIMA ALVES <52918563862@info.local>
2025-12-03 10:48:13 -0300

    Ajustes JavaScript

--- Back-End/wwwroot/js/site.js
+++ Back-End/wwwroot/js/site.js
@@ -1,9 +1,12 @@
 ﻿const aparecerLayout = document.getElementById('add-baixo-esq');
 const aparecerLayoutBotao = document.getElementById('edit-add-layer');
 const botaoAdicionar = document.getElementById('botao-add');
+const botaoAdicionar2 = document.getElementById('botao-add2');
 const aparecerLayoutExcluir = document.getElementById('add-baixo-dir');
 const botaoExcluir = document.getElementById('botao-excluir');
-const botaoCancelar = document.getElementById('botao-cancelar');
+const botaoCancelar1 = document.getElementById('botao-cancelar1');
+const botaoCancelar2 = document.getElementById('botao-cancelar2');
+const botaoEditar = document.getElementById('botao-editar');
 
 botaoAdicionar.addEventListener('click', function () {
     if (aparecerLayout.style.display === 'none' || !aparecerLayout.style.display) {
@@ -22,6 +25,17 @@ botaoExcluir.addEventListener('click', function () {
     }
 });
 
-botaoCancelar.addEventListener('click', function () {
-    aparecerLayoutExcluir.style.display = 'none';
+botaoCancelar1.addEventListener('click', function () {
+    aparecerLayout.style.display = 'none';
 });
+
+botaoCancelar2.addEventListener('click', function () {
+    aparecerLayoutExcluir.style.display = 'none';
+}); 
+
+botaoEditar.addEventListener('click', function () {
+        if(aparecerLayout.style.display === 'none' || !aparecerLayout.style.display) {
+            aparecerLayout.style.display = 'block';
+            botaoAdicionar2.textContent = "Editar Pokemon";
+        }
+    });
