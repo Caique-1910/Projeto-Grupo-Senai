67f54cbff5a19647cb5ff9431d2b78b5f427ca5d
Eduardo-Bernal <cbedudu@gmail.com>
2025-12-05 21:21:24 -0300

    Simples correcao da parte de serializar o JSON

--- Back-End/obj/Debug/net9.0/BackEndProjeto.AssemblyInfo.cs
+++ Back-End/obj/Debug/net9.0/BackEndProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEndProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e81d71e3cb80d9a08126b823c89dd1612cbc479")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a671daf0bdc6aed115e04c0fa86f8a961b97c11")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEndProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEndProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
