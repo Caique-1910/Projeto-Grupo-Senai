c8d508aafcde521dfe395223a45132a2505fdb5b
Eduardo-Bernal <cbedudu@gmail.com>
2025-12-08 21:08:51 -0300

    Funcionalidade - Sair/Session.Clear

--- Back-End/obj/Debug/net9.0/BackEndProjeto.AssemblyInfo.cs
+++ Back-End/obj/Debug/net9.0/BackEndProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEndProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06689bdead38ab744969feeece70fa70884222f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b35b4606e980b3c7a9e5d72e86acf36c38afe5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEndProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEndProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
